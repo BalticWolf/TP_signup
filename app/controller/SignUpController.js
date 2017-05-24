d6731d03e4f030cf4ceda4681ab0a1f3e3d293da
Sarah Drapeau <sarah.drapeau@alumni-imie.fr>
2017-05-24 16:19:12 +0200

    upgrade: register a user without destroying the user.cv

--- app/controller/SignUpController.js
+++ app/controller/SignUpController.js
@@ -21,7 +21,7 @@ class SignUpController {
 	  	const fs = require('fs');
 	  	const csvFile = this._path.join('data', 'user.csv');
 
-	 	  fs.writeFile(csvFile, user, (err) => {
+	 	  fs.appendFile(csvFile, user, (err) => {
           if (err) throw err;
           console.log('The file has been saved!');
 		  });
