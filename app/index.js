16bed97b9583d009a92781cd0981f8542a0eeb69
bricePillet121 <bricepillet@gmail.com>
2017-05-24 11:55:11 +0200

    userController and layoutAction created!

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 
 const SignUpController = require('./controller/SignUpController');
 const LoginController = require('./controller/LoginController');
-
+const UserController = require ('./controller/UserController');
 const port = process.argv[2] || 3000;
 const app = express();
 
@@ -20,3 +20,45 @@ app.post('/signup', signUpCtrl.postSignUpAction);
 app.post('/login', loginCtrl.postLoginAction);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+
+/*fs.open(configPathFile, 'r', (err, fd) => {
+    if (err) {
+        if(err.code !== 'ENOENT') { }
+        } else {
+        console.log("Je supprime");
+        fs.unlinkSync(configPathFile);
+        }
+
+
+
+        console.log("Je crée");
+        fs.readFile(sampleConfigPathFile, 'utf8', (err, contentSampleFile) => {
+            if (err) throw err;
+            // First
+            // contentSampleFile = contentSampleFile.replace("PORT", args.port)
+            //     .replace("HOST", args.host)
+            //     .replace("PROTOCOL", args.protocol);
+            // console.log(contentSampleFile);
+
+            // Second
+            // const objConfig = JSON.parse(contentSampleFile);
+            //
+            // objConfig.port = args.port;
+            // objConfig.host = args.host;
+            // objConfig.protocol = args.protocol;
+            //
+            // console.log(JSON.stringify(objConfig));
+
+            // Par tableau associatif ["port", "host", "protocol"]
+        Object.keys(args).forEach(key => {
+            contentSampleFile = contentSampleFile.replace(key.toUpperCase(), args[key])
+        });
+
+        fs.writeFile(configPathFile, contentSampleFile, 'utf8', (err) => {
+            if (err) throw err;
+
+        console.log("Le fichier de configuration est créé.");
+        });
+    });
+});*/
