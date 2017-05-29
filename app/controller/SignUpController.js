c001f2d11f22e046f721d608c62158ffd623d044
Sarah Drapeau <sarah.drapeau@alumni-imie.fr>
2017-05-29 10:22:57 +0200

    create userTableAction.pug

--- app/controller/SignUpController.js
+++ app/controller/SignUpController.js
@@ -8,23 +8,23 @@ class SignUpController {
     }
 
     postSignUpAction(req, res) {
-    	// données récupérées du post
-	    let name = req.body.name;
-	  	let email = req.body.email;
-	  	let password = req.body.password;
-	  	let confirmPassword = req.body.confirmPassword;
-	  	
+      // données récupérées du post
+      let name = req.body.name;
+      let email = req.body.email;
+      let password = req.body.password;
+      let confirmPassword = req.body.confirmPassword;
+      
       // concaténation des données
-	  	let user = name +","+ email +","+ password +","+ confirmPassword +"\n"
-	 		 	
-	  	// écriture des données dans un fichier user.csv
-	  	const fs = require('fs');
-	  	const csvFile = this._path.join('data', 'user.csv');
+      let user = name +","+ email +","+ password +","+ confirmPassword +"\n"
+        
+      // écriture des données dans un fichier user.csv
+      const fs = require('fs');
+      const csvFile = this._path.join('data', 'user.csv');
 
-	 	  fs.appendFile(csvFile, user, (err) => {
+      fs.appendFile(csvFile, user, (err) => {
           if (err) throw err;
           console.log('The file has been saved!');
-		  });
+      });
       
       res.render('loginAction');
     }
